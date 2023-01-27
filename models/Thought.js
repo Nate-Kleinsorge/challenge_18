fc4ee3b1723ab1706bad9edb5f2d590652d9e434
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-01-27 12:14:13 -0500

    added files and models

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: True,
+            ref: 'User',
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        timestamps: true,
+        toJSON: { getters: true }
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
